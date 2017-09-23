b3ca3d79a1e296340db865410a54f1b53fae3bf1
Annie Steenson <anniesteenso@gmail.com>
2017-09-23 02:13:42 -0400

    Added firebase auth

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const path = require('path'),
   express = require('express'),
-  app = express();
+  app = express(),
+	db = require('./db');
 
 app.use(express.static(path.resolve('public')));
 app.set('view engine', 'html');
+
+app.post('/api/newuser', (req, res) => {
+	console.log('/api/newuser');
+	console.log(req);
+	//db.createUser();
+});
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve('public', 'index.html'));
---
+++ server/db.js
@@ -0,0 +1,28 @@
+const firebase = require('firebase'),
+	config = {
+		apiKey: "AIzaSyBXqb4N2Kgdi5hZFvzVcHjenqPFOFZw1J8",
+		authDomain: "jack-my-swag.firebaseapp.com",
+		databaseURL: "https://jack-my-swag.firebaseio.com",
+		projectId: "jack-my-swag",
+		storageBucket: "",
+		messagingSenderId: "826921485870"
+	};
+
+firebase.initializeApp(config);
+
+module.exports = {
+	createUser: (email, password) => {
+		firebase.auth().createUserWithEmailAndPassword(email, password)
+			.catch(error => {
+				console.log('Error createUserWithEmailAndPassword');
+				console.log(error);
+			});
+	},
+	login: (email, password) => {
+		firstbase.auth().signInWithEmailAndPassword(email, password)
+			.catch(error => {
+				console.log('Error signInWithEmailAndPassword');
+				console.log(error);
+			});
+	}
+};
